09d6ff626c6bcf4a8bc1cd8cedc2747e45a1d3fd
M95 <tommynara@gmail.com>
2025-03-28 13:30:36 +0900

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,13 +1,56 @@
-CREATE DATABASE IF NOT EXISTS test;
-USE test;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 3.35.233.100    Database: memnode
+-- ------------------------------------------------------
+-- Server version	8.0.41-0ubuntu0.24.04.1
 
-CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(100) NOT NULL,
-  email VARCHAR(100) UNIQUE NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- 테스트 데이터 삽입
-INSERT INTO users (name, email) VALUES
-  ('테스트 사용자', 'test@example.com'); 
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `phone` varchar(45) DEFAULT NULL,
+  `birthdate` date DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'이진우','test@example.com','2025-03-25 08:54:16','010-2222-2222','2025-03-25'),(2,'김태현','tommy@m95.co.kr','2025-03-26 07:14:59','010-00000000','2025-03-25'),(4,'test','test@test.com','2025-03-26 07:16:13','010-7679-8534','2025-03-26'),(5,'전송이','songyi.9801@daum.net','2025-03-26 07:16:32','010-2288-0733','1998-01-13'),(7,'오유진','dngp@gks.com','2025-03-26 07:17:00','010-1111-2223','2025-03-05'),(8,'김건희','k1212gh@gmail.com','2025-03-26 07:17:07','010-4559-8578','2000-12-12'),(9,'전윤재','kevin7609@naver.com','2025-03-26 07:17:15','010-7184-7619','2002-11-15'),(10,'양경윤','yky0306129@gmail.com','2025-03-26 07:17:50','010-5688-6231','2003-06-12'),(11,'황동건','guugun@naver.com','2025-03-26 07:18:04','010-9876-1234','2025-02-20'),(12,'이도림','dorim@hancom.com','2025-03-26 07:18:41','010-0000-0000','2001-03-25'),(13,'1','1@1','2025-03-26 07:19:54','11-111-1111','1111-11-11'),(14,'222','222@222.com','2025-03-26 07:20:19','222-2222-2222','2222-02-02');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-28 13:28:38
